6e0b1ba61732a0b00c3abae020d482859b6b31f7
ImNicknamez <136889440+ImNicknamez@users.noreply.github.com>
2023-07-30 16:23:46 +0200

    Update 16x16.lua

--- 16x16.lua
+++ 16x16.lua
@@ -4,7 +4,7 @@
  local Workspace = game:GetService("Workspace")
 
 			--// importing the textures
-			local objs = game:GetObjects("rbxassetid://14179748084")
+			local objs = game:GetObjects("rbxassetid://14179004161")
 			local import = objs[1]
 
 			import.Parent = game:GetService("ReplicatedStorage")
@@ -45,56 +45,45 @@
 			}
 
 			--// main viewmodel renderer
-			local func = Workspace:WaitForChild("Camera").Viewmodel.ChildAdded:Connect(function(tool)
-				
-				if(not tool:IsA("Accessory")) then return end
-				
-				for i,v in pairs(index) do
-				
-					if(v.name == tool.Name) then
-					
-						for i,v in pairs(tool:GetDescendants()) do
-				
-							if(v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation")) then
-							
-								v.Transparency = 1
-							
+					local func = Workspace:WaitForChild("Camera").Viewmodel.ChildAdded:Connect(function(tool)
+						if(not tool:IsA("Accessory")) then return end
+						for i,v in pairs(index) do
+							if(v.name == tool.Name) then
+								for i,v in pairs(tool:GetDescendants()) do
+									if(v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation")) then
+										v.Transparency = 1
+										v.CanCollide = false
+									end
+								end
+								
+								local model = v.model:Clone()
+								model.CFrame = tool:WaitForChild("Handle").CFrame * v.offset
+								model.CFrame *= CFrame.Angles(math.rad(0),math.rad(-50),math.rad(0))
+								model.Parent = tool
+								
+								local weld = Instance.new("WeldConstraint",model)
+								weld.Part0 = model
+								weld.Part1 = tool:WaitForChild("Handle")
+								
+								local tool2 = Players.LocalPlayer.Character:WaitForChild(tool.Name)
+								for i,v in pairs(tool2:GetDescendants()) do
+									if(v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation")) then
+										v.Transparency = 1
+										v.CanCollide = false
+									end
+								end
+								
+								local model2 = v.model:Clone()
+								model2.Anchored = false
+								model2.CanCollide = false
+								model2.CFrame = tool2:WaitForChild("Handle").CFrame * v.offset
+								model2.CFrame *= CFrame.Angles(math.rad(0),math.rad(-50),math.rad(0))
+								model2.CFrame *= CFrame.new(0.4,0,-.9)
+								model2.Parent = tool2
+								
+								local weld2 = Instance.new("WeldConstraint",model)
+								weld2.Part0 = model2
+								weld2.Part1 = tool2:WaitForChild("Handle")
 							end
-						
 						end
-					
-						local model = v.model:Clone()
-						model.CFrame = tool:WaitForChild("Handle").CFrame * v.offset
-						model.CFrame *= CFrame.Angles(math.rad(0),math.rad(-50),math.rad(0))
-						model.Parent = tool
-						
-						local weld = Instance.new("WeldConstraint",model)
-						weld.Part0 = model
-						weld.Part1 = tool:WaitForChild("Handle")
-						
-						local tool2 = Players.LocalPlayer.Character:WaitForChild(tool.Name)
-						
-						for i,v in pairs(tool2:GetDescendants()) do
-				
-							if(v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation")) then
-							
-								v.Transparency = 1
-							
-							end
-						
-						end
-						
-						local model2 = v.model:Clone()
-						model2.Anchored = false
-						model2.CFrame = tool2:WaitForChild("Handle").CFrame * v.offset
-						model2.CFrame *= CFrame.Angles(math.rad(0),math.rad(-50),math.rad(0))
-						model2.CFrame *= CFrame.new(1,0,-.9)
-						model2.Parent = tool2
-						
-						local weld2 = Instance.new("WeldConstraint",model)
-						weld2.Part0 = model2
-						weld2.Part1 = tool2:WaitForChild("Handle")
-					
-					end
-				end
-			end)
+					end)
